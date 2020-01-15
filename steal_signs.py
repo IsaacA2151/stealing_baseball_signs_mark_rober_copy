8b86d72125be8206cc2f8bce3c69ab0212b10f52
IsaacA2151 <46939800+IsaacA2151@users.noreply.github.com>
2020-01-15 21:01:55 +0000

    Create steal_signs.py

---
+++ steal_signs.py
@@ -0,0 +1,77 @@
+import numpy as np
+import random
+import decimal
+
+def seqToBin(actions,seq,show=False):
+    seqBin = []
+    for i in range(len(seq)):
+        seqBin.append([0 for i in range(len(actions))])
+        for k in range(len(seq[i])):
+            if seq[i][k] in actions:
+                seqBin[i][actions.index(seq[i][k])] = 1
+                
+    seqBin = np.asarray(seqBin)
+    return seqBin                
+
+def findSign(weights,actions):
+    largest = weights[0]
+    ind = 0
+    for i in range(len(weights)):
+        if weights[i] > largest:
+            largest = weights[i]
+            ind = i
+            
+    return actions[ind]
+            
+
+'''actions = ['eye','mouth','nose','ear']
+seq = [['eye','mouth','ear'],
+       ['eye','mouth','nose'],
+       ['eye','nose','ear'],
+       ['mouth','nose'],
+       ['nose'],
+       ['ear','mouth'],
+       ['ear','nose'],
+       ['mouth','nose','ear'],
+       ['eye','nose','mouth']]'''
+       
+actions = ['T','M','O','R','N','I','E','S','Y']
+seq = [['I','S','E','R','M','O','N'],
+       ['N','I','R','T','O','N','S'],
+       ['M','O','R','N','I','E','S'],
+       ['S','M','I','R','O','T','E'],
+       ['N','E','S','T','R','I','M'],
+       ['T','R','O','M','I','S','Y'],
+       ['M','E','R','T','O','Y','S'],
+       ['E','Y','N','O','M','I','T']]
+
+############################################ Mouth means steal ############################################
+
+seqBin = seqToBin(actions,seq)
+
+#steal_or_not = np.array([1,1,0,1,0,1,0,1,1])
+steal_or_not = np.array([1,1,1,0,1,0,0,1])
+weights = np.array([float(random.uniform(-1,1)) for i in range(len(actions))])
+alpha = 0.1
+
+input = seqBin[0]
+goal = steal_or_not[0]
+
+for it in range(100):
+    for row in range(len(steal_or_not)):
+        
+        input = seqBin[row]
+        goal = steal_or_not[row]
+        pred = input.dot(weights)
+        
+        error = (goal - pred) ** 2
+        delta = pred - goal
+        
+        weights = weights - (alpha * (input * delta))
+        
+    print('Prediction: ' + str(pred))
+    
+
+print(weights)
+print(findSign(weights,actions))
+    
